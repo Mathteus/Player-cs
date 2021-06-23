3b157a9e36c9016da8325355c0365aed90aa9236
Matheus Henrique <85768227+Mathteus@users.noreply.github.com>
2021-06-22 23:58:33 -0300

    Add files via upload

---
+++ PlayerC/PlayerC/PlayerC/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace PlayerC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.player = new AxWMPLib.AxWindowsMediaPlayer();
+            this.lstPlayList = new System.Windows.Forms.ListBox();
+            this.btnAbrir = new System.Windows.Forms.Button();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.btnCarregar = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.ofdAbrir = new System.Windows.Forms.OpenFileDialog();
+            this.sfdSalvar = new System.Windows.Forms.SaveFileDialog();
+            this.buttonEliminar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // player
+            // 
+            this.player.Enabled = true;
+            this.player.Location = new System.Drawing.Point(12, 12);
+            this.player.Name = "player";
+            this.player.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("player.OcxState")));
+            this.player.Size = new System.Drawing.Size(486, 253);
+            this.player.TabIndex = 0;
+            this.player.KeyDownEvent += new AxWMPLib._WMPOCXEvents_KeyDownEventHandler(this.player_KeyDownEvent);
+            this.player.KeyPressEvent += new AxWMPLib._WMPOCXEvents_KeyPressEventHandler(this.player_KeyPressEvent);
+            // 
+            // lstPlayList
+            // 
+            this.lstPlayList.FormattingEnabled = true;
+            this.lstPlayList.Location = new System.Drawing.Point(12, 271);
+            this.lstPlayList.Name = "lstPlayList";
+            this.lstPlayList.Size = new System.Drawing.Size(486, 160);
+            this.lstPlayList.TabIndex = 1;
+            this.lstPlayList.DoubleClick += new System.EventHandler(this.LstPlayList_DoubleClick);
+            // 
+            // btnAbrir
+            // 
+            this.btnAbrir.Location = new System.Drawing.Point(12, 437);
+            this.btnAbrir.Name = "btnAbrir";
+            this.btnAbrir.Size = new System.Drawing.Size(75, 23);
+            this.btnAbrir.TabIndex = 2;
+            this.btnAbrir.Text = "Abrir";
+            this.btnAbrir.UseVisualStyleBackColor = true;
+            this.btnAbrir.Click += new System.EventHandler(this.BtnAbrir_Click);
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(93, 437);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(75, 23);
+            this.btnSalvar.TabIndex = 3;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.BtnSalvar_Click);
+            // 
+            // btnCarregar
+            // 
+            this.btnCarregar.Location = new System.Drawing.Point(174, 437);
+            this.btnCarregar.Name = "btnCarregar";
+            this.btnCarregar.Size = new System.Drawing.Size(75, 23);
+            this.btnCarregar.TabIndex = 4;
+            this.btnCarregar.Text = "Carregar";
+            this.btnCarregar.UseVisualStyleBackColor = true;
+            this.btnCarregar.Click += new System.EventHandler(this.BtnCarregar_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(255, 437);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpar.TabIndex = 5;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.BtnLimpar_Click);
+            // 
+            // ofdAbrir
+            // 
+            this.ofdAbrir.Multiselect = true;
+            // 
+            // buttonEliminar
+            // 
+            this.buttonEliminar.Location = new System.Drawing.Point(336, 437);
+            this.buttonEliminar.Name = "buttonEliminar";
+            this.buttonEliminar.Size = new System.Drawing.Size(75, 23);
+            this.buttonEliminar.TabIndex = 6;
+            this.buttonEliminar.Text = "Eliminar";
+            this.buttonEliminar.UseVisualStyleBackColor = true;
+            this.buttonEliminar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(509, 482);
+            this.Controls.Add(this.buttonEliminar);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnCarregar);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.btnAbrir);
+            this.Controls.Add(this.lstPlayList);
+            this.Controls.Add(this.player);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Player";
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer player;
+        private System.Windows.Forms.ListBox lstPlayList;
+        private System.Windows.Forms.Button btnAbrir;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Button btnCarregar;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.OpenFileDialog ofdAbrir;
+        private System.Windows.Forms.SaveFileDialog sfdSalvar;
+        private System.Windows.Forms.Button buttonEliminar;
+    }
+}
+
---
+++ PlayerC/PlayerC/PlayerC/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace PlayerC
+{
+    public partial class Form1 : Form
+    {
+        bool tocando = false;
+        public Form1()
+        {
+            InitializeComponent();
+            Form f = Form1.ActiveForm;
+            
+        }
+
+        WMPLib.IWMPPlaylist playList;
+        
+        private void BtnAbrir_Click(object sender, EventArgs e)
+        {
+            ofdAbrir.Title = "Abrir mídia";
+            ofdAbrir.Filter = "Arquivo mp4|*.mp4|Arquvio mp3|*.mp3";
+           if (ofdAbrir.ShowDialog() == DialogResult.OK )
+            {
+                playList = player.playlistCollection.newPlaylist("Lista");
+
+                foreach (var arquivo in ofdAbrir.FileNames)
+                {
+                    playList.appendItem(player.newMedia(arquivo));
+                    lstPlayList.Items.Add(arquivo);
+
+
+                    player.currentPlaylist = playList;
+                    //player.Ctlcontrols.play();
+                }
+            }
+        }
+
+        private void BtnSalvar_Click(object sender, EventArgs e)
+        {
+            if(lstPlayList.Items.Count > 0)
+            {
+                sfdSalvar.Title = "Salvar PlayList";
+                sfdSalvar.Filter = "Arquivo texto|*.txt";
+                if(sfdSalvar.ShowDialog() == DialogResult.OK)
+                {
+                    StreamWriter arquivo = new StreamWriter(sfdSalvar.FileName, false);
+                    for (int i = 0; i < lstPlayList.Items.Count ; i++)
+                    {
+                        arquivo.WriteLine(lstPlayList.Items[i].ToString());
+                    }
+                    arquivo.Close();
+                }
+                
+            }
+            
+
+        }
+
+        private void BtnCarregar_Click(object sender, EventArgs e)
+        {
+            ofdAbrir.Title = "Abrir PlayList";
+            ofdAbrir.Filter = "Arquivo texto|*.txt";
+            ofdAbrir.Multiselect = false;
+            if (ofdAbrir.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader arquivo = new StreamReader(ofdAbrir.FileName);
+                while (arquivo.Peek() != -1)
+                {
+                    lstPlayList.Items.Add(arquivo.ReadLine());
+                }
+                arquivo.Close();
+            }
+        }
+
+        private void LstPlayList_DoubleClick(object sender, EventArgs e)
+        {
+            if (lstPlayList.Items.Count > 0)
+            {
+                player.URL = lstPlayList.SelectedItem.ToString();
+                player.Ctlcontrols.play();
+                tocando = true;
+            }
+        }
+
+        private void BtnLimpar_Click(object sender, EventArgs e)
+        {
+            player.Ctlcontrols.stop();
+            lstPlayList.Items.Clear();
+        }
+
+        /*private void fullScreen(object o, KeyPressEventArgs b)
+        {
+            
+            if (b.KeyChar == (char)Keys.F & player.fullScreen == true)
+            {
+                o.fullScreen = false;
+            }
+            else if (b.KeyChar == (char)Keys.F & player.fullScreen == false)
+            {
+                o.fullScreen=true;
+            }
+        }*/
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int selecionado=lstPlayList.SelectedIndex;
+            lstPlayList.Items.RemoveAt(selecionado);
+        }
+
+        private void player_KeyPressEvent(object sender, AxWMPLib._WMPOCXEvents_KeyPressEvent e)
+        {
+            if(e.nKeyAscii == 102 & player.fullScreen == true)
+            {
+                player.fullScreen = false;
+            }
+            else if(e.nKeyAscii == 102 & player.fullScreen == false)
+            {
+                player.fullScreen = true;
+            }
+
+            if(tocando & e.nKeyAscii == 107)
+            {
+                player.Ctlcontrols.pause();
+                tocando = !tocando;
+            }
+            else if(!tocando & e.nKeyAscii == 107)
+            {
+                player.Ctlcontrols.play();
+                tocando = !tocando;
+            }
+
+            if(e.nKeyAscii == 97)
+            {
+                player.Ctlcontrols.previous();
+            }
+            if(e.nKeyAscii == 100)
+            {
+                player.Ctlcontrols.next();
+            }
+
+            if(e.nKeyAscii == 48)
+            {
+                MessageBox.Show(player.Ctlcontrols.currentPosition.ToString());
+            }
+        }
+
+        private void player_KeyDownEvent(object sender, AxWMPLib._WMPOCXEvents_KeyDownEvent e)
+        {
+            if(tocando & e.nKeyCode == 74)
+            {
+                double t = ((player.currentMedia.duration/100)*1);
+                player.Ctlcontrols.currentPosition -= t;
+            }
+
+            if(tocando & e.nKeyCode == 76)
+            {
+                double t = ((player.currentMedia.duration / 100) * 1);
+                player.Ctlcontrols.currentPosition += t;
+            }
+        }
+    }
+}
